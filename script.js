ffbaf220aaaa7348dd9e817f2676591e1e71dc90
Anupama_Halder <anupamahalder2022@gmail.com>
2023-08-29 11:18:02 +0530

    Chaged text color according to the background color

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 window.onload = () => {
     main();
 }
+const textHeading = document.getElementById('heading-text');
 function main(){
     // get the div container 
     const mainDiv = document.getElementById('div-id');
@@ -25,14 +26,22 @@ function generateRgbColor(){
     //Math.random() gives the value from 0 to 1 in fraction mode
     //to get integer we can use floor()
     // 1. generate random color for red
-    const red = Math.floor(Math.random()*255)
+    const red = Math.floor(Math.random()*255);
     // 2. generate random color for green
-    const green = Math.floor(Math.random()*255)
+    const green = Math.floor(Math.random()*255);
     // 2. generate random color for blue
-    const blue = Math.floor(Math.random()*255)
-
+    const blue = Math.floor(Math.random()*255);
+    //change text color when background color is too dark
+    if((red <= 80 && green <= 80 && blue <= 80) || (red<=10 || green<=10 || blue<=10)){
+        textHeading.style.color = 'white';
+    }
+    else{
+        textHeading.style.color = 'black';
+    }
     // Make rgb string with template string
-    return `rgb(${red},${green},${blue})`;
+    const rgbColor = `rgb(${red},${green},${blue})`;
+    console.log(rgbColor);
+    return rgbColor;
 }
 // Step-3: Collect all necessary Element by selector in main function
 
