be6b6a013ece07081cce1121bc924732f0b90017
Anupama_Halder <anupamahalder2022@gmail.com>
2023-08-29 10:01:51 +0530

    Added js file to create random generated rgb color change function

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+    Project Requirements:
+        Change the background color by the random generating rgb color by clicking a button
+*/
+// Steps to follow 
+// Step-1: create onload handler
+window.onload = () => {
+    main();
+}
+function main(){
+
+}
+// Step-2: create random color generated function 
+function generateRgbColor(){
+    
+}
+// Step-3: Collect all necessary Element by selector 
+
+// Setp-4: Handle event 
